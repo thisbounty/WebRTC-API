b52942025b45835fe46424634e0f3ecb9bc65dd7
Steve <urgent.programmer@gmail.com>
2016-11-07 15:16:20 -0500

    Tokbox -- call/new endpoint
    Created new endpoint for a new call
    
    http://loopback.io/doc/en/lb2/Remote-methods.html\#returning-data-outside-of-a-json-field

--- common/models/call.js
+++ common/models/call.js
@@ -2,12 +2,16 @@
 
 module.exports = function (Post) {
 
-  Post.createFakeData = function (faker) {
-    return Post.create({
-      title: faker.lorem.sentence(),
-      content: faker.lorem.paragraph(),
-      image: faker.image.imageUrl() + '/nature/' + (Math.random() * 9 | 0)
-    });
+  Call.new= function () {
   };
+
+  Call.remoteMethod('new', {
+    http: {path: '/new', verb: 'get'},
+    accepts: [
+     {arg: 'req', type: 'object', 'http': {source: 'req'}},
+     {arg: 'res', type: 'object', 'http': {source: 'res'}}
+    ],
+    returns: {arg:'call',type:'array'}
+  });
 
 };
